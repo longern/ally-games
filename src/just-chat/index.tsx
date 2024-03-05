f70597742ddebbf581aeea1c0287e1aeb40488d6
longern <771815005@qq.com>
2024-03-05 23:10:06 +0800

    Display chat messages

--- src/just-chat/index.tsx
+++ src/just-chat/index.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import {
+  Avatar,
+  Box,
   Button,
+  Card,
+  Container,
   CssBaseline,
   Stack,
+  TextField,
   ThemeProvider,
+  Typography,
   createTheme,
 } from "@mui/material";
 
@@ -17,18 +23,85 @@ const GameBoard: GameBoardComponent<typeof game> = ({
   chatMessages,
   sendChatMessage,
 }) => {
+  const [userInput, setUserInput] = useState("");
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+      sendChatMessage(userInput);
+      setUserInput("");
+    },
+    [sendChatMessage, userInput]
+  );
+
   return (
-    <Stack>
-      <Stack sx={{ flexGrow: 1, overflowY: "auto" }}>
-        {chatMessages.map((message) => (
-          <div key={message.id}>
-            {message.sender}: {message.payload}
-          </div>
-        ))}
-      </Stack>
-      <Button sx={{ flexShrink: 0 }} onClick={() => sendChatMessage("test")}>
-        Send
-      </Button>
+    <Stack sx={{ height: "100vh" }}>
+      <Box sx={{ flexGrow: 1, overflowY: "auto" }}>
+        <Container maxWidth="md" sx={{ paddingY: 1 }}>
+          <Stack spacing={2}>
+            {chatMessages.map((message) => (
+              <Stack
+                key={message.id}
+                gap={2}
+                sx={{
+                  flexDirection:
+                    message.sender !== playerID ? "row" : "row-reverse",
+                  "&>*": { flexShrink: 0 },
+                }}
+              >
+                <Avatar />
+                <Stack
+                  sx={{
+                    flex: "1 1",
+                    minWidth: 0,
+                    alignItems: message.sender !== playerID ? "start" : "end",
+                  }}
+                >
+                  <Typography
+                    variant="subtitle2"
+                    color="text.secondary"
+                    sx={{
+                      paddingX: "0.5em",
+                      maxWidth: "100%",
+                      overflow: "hidden",
+                      textWrap: "nowrap",
+                      textOverflow: "ellipsis",
+                    }}
+                  >
+                    {message.sender}
+                  </Typography>
+                  <Card
+                    sx={{
+                      backgroundColor: "primary.main",
+                      borderRadius: "4px",
+                      padding: "0.5em 0.8em",
+                      overflowWrap: "anywhere",
+                    }}
+                  >
+                    {message.payload}
+                  </Card>
+                </Stack>
+                <Box sx={{ width: "60px" }} />
+              </Stack>
+            ))}
+          </Stack>
+        </Container>
+      </Box>
+      <Container maxWidth="md">
+        <form onSubmit={handleSubmit}>
+          <Stack direction="row" sx={{ paddingY: 0.5 }}>
+            <TextField
+              fullWidth
+              size="small"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
+            <Button type="submit" sx={{ flexShrink: 0 }}>
+              Send
+            </Button>
+          </Stack>
+        </form>
+      </Container>
     </Stack>
   );
 };
